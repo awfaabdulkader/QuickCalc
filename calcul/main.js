cbccada064f4f5078e5d7d552d6bae9770e9496c
AWFA ABDULKADER <114150913+awfaabdulkader@users.noreply.github.com>
2023-09-01 22:57:31 +0100

    Add files via upload

---
+++ calcul/main.js
@@ -0,0 +1,14 @@
+let BtnInput = document.getElementsByClassName('btninput');
+let BtnDiv = document.getElementsByClassName('btnI');
+let Button = document.getElementById('button');
+let calc = document.getElementById('inputcalc');
+let one = document.getElementById('one')
+let two = document.getElementsByClassName('two')
+let three = document.getElementsByClassName('three')
+
+
+if (calc.value == 0) {
+    // Set calc's value to 0 if it's currently equal to 0
+    calc.value = 0;
+}
+
